d233da8adb7e061cb4ec623115a5b89a8fb8fa38
misinayu <yama_masa22@yahoo.co.jp>
2018-12-30 15:53:34 +0000

    Create new pages

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/hello', function () {
+//    return '<h1>Hello World</h1>';
+//});
+
+//Route::get('/about', function () {
+//    return view('pages.about');
+//});
+
+//Route::get('/users/{id}/{name}', function ($id, $name) {
+//    return 'This is user ' . $name . ' with an id of ' . $id;
+//});
+
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
