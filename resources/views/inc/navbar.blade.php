7b0d3130e4988a1057e3b7049ddc39bab198942a
misinayu <yama_masa22@yahoo.co.jp>
2019-01-03 12:26:39 +0000

    Change posts create page

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -27,6 +27,9 @@
                 </div>
             </li>
         </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a class="nav-link" href="/posts/create">Create Post</a></li>
+        </ul>
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
