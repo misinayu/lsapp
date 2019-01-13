d85df31a67891f437522f3e935476bcba905ee39
misinayu <yama_masa22@yahoo.co.jp>
2019-01-13 15:21:45 +0000

    Add dashboard show
    Can edit only my posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,7 +6,7 @@
         @foreach($posts as $post)
             <div class="well">
                 <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
-                <small>Writter on {{$post->created_at}}</small>
+                <small>Writter on {{$post->created_at}} by {{$post->user->name}}</small>
             </div>
         @endforeach
         {{$posts->links()}}
